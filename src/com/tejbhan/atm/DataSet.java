aad5d81b47fdea9fe01c1deeecf342e20d101ddc
Amit Maurya <amit.maurya.507464@gmail.com>
2020-10-14 17:04:58 +0530

    First Commit

---
+++ src/com/tejbhan/atm/DataSet.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tejbhan.atm;
+
+/**
+ *
+ * @author Amit Maurya
+ */
+public class DataSet {
+    
+    public static double balance = 10000;
+    public static double lastTransaction = 0;
+    public static String transaction = "";
+}
